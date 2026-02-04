b35359425b7706604279460065b642b3fd611572
AntonBelikov <k4rtovan@yandex.ru>
2026-02-04 18:12:40 +0400

    исправить отступ

--- src/ru/yandex/practicum/WordInDictionaryExseption.java
+++ src/ru/yandex/practicum/WordInDictionaryExseption.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum;
 
-public class WordInDictionaryExseption extends Exception{
+public class WordInDictionaryExseption extends Exception {
     public WordInDictionaryExseption(final String message) {
         super(message);
     }
